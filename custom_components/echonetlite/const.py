d65a9a7e9d59c00b421ece44bdae7bb12c512d0e
carlito1979 <carlitoandersson@gmail.com>
2021-07-18 08:15:34 +1200

    Customised Swing Control
    Adds code to allow use of Preset function in HA Climate entity to control the Horizontal airflow modes (along with Swing to control Vertical airflow)
    Also allows control of Auto Position (for Vertical only) and continuous Swing (for both vanes) directly from Vertical (Swing) and Horizontal (Preset) options
    This allows fine control of your heat-pump airflow within the climate entity without resorting to multiple select entities.

--- custom_components/echonetlite/const.py
+++ custom_components/echonetlite/const.py
@@ -6,13 +6,20 @@ DOMAIN = "echonetlite"
 
 SENSOR_TYPE_TEMPERATURE = "temperature"
 
+HVAC_OP_FAN = "Air flow rate setting"
+HVAC_OP_AUTO = "Automatic control of air flow direction setting"
+HVAC_OP_SWING = "Automatic swing of air flow setting"
+HVAC_OP_HORIZ = "Air flow direction (horizontal) setting"
+HVAC_OP_VERT = "Air flow direction (vertical) setting"
+
+
 HVAC_SELECT_OP_CODES = {
-        0xA0: {"name": "Air flow rate setting", "options": FAN_SPEED},
-        0xA1: {"name": "Automatic control of air flow direction setting", "options": AUTO_DIRECTION},
-        0xA3: {"name": "Automatic swing of air flow setting", "options": SWING_MODE},
-        0xA5: {"name": "Air flow direction (horizontal) setting", "options": AIRFLOW_HORIZ},
-        0xA4: {"name": "Air flow direction (vertical) setting", "options": AIRFLOW_VERT}
-        }
+        0xA0: {"name": HVAC_OP_FAN, "options": FAN_SPEED},
+        0xA1: {"name": HVAC_OP_AUTO, "options": AUTO_DIRECTION},
+        0xA3: {"name": HVAC_OP_SWING, "options": SWING_MODE},
+        0xA5: {"name": HVAC_OP_HORIZ, "options": AIRFLOW_HORIZ},
+        0xA4: {"name": HVAC_OP_VERT, "options": AIRFLOW_VERT}
+    }
 
 ENL_SENSOR_OP_CODES = {
         0x00: {
@@ -27,8 +34,19 @@ ENL_SENSOR_OP_CODES = {
             }
         },
         'default':  {CONF_ICON: None, CONF_TYPE: None},
-    }
+    }  
 
 ATTR_STATE_ON = "on"
 ATTR_STATE_OFF = "off"
+
+HVAC_AUTO_SWING_VERT = "auto-vert"
+HVAC_AUTO_SWING_BOTH = "auto"
+HVAC_AUTO_SWING_HORIZ = "auto-horiz"
+HVAC_AUTO_SWING_OFF = "non-auto"
+
+HVAC_SWING_BOTH = "vert-horiz"
+HVAC_SWING_VERT = "vert"
+HVAC_SWING_HORIZ = "horiz"
+HVAC_SWING_OFF = "not-used"
+HVAC_SWING_SPLIT = "left-right"
 
